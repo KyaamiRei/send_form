b76a9546b3f05968c5596e2b53b2636cee2b4c93
kyaami <kolyavasilenko2703@gmail.com>
2022-11-14 18:25:38 +0300

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import { useState } from 'react';
 
+// подключени еaxios для отправки запросов на сервер
+// import axios from 'axios';
+
+// подключение иконок font-awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+// подключение стилей
 import styles from './App.module.scss';
 
 export const App: React.FC = () => {
+  //состояние для текста в input
   const [inputValue, setInputValue] = useState<string>('');
 
-  const onClickSend = () => {
+  // функция для отправки данных по нажатию на кнопку
+  const onClickSend: () => void = () => {
+    try {
+      // отправка формы
+      // axios.post()
+      setInputValue('');
+      alert('Форма успешно отправлена');
+    } catch (error) {
+      // действия в случае ошибки при отправке формы
+      alert('Произошла ошибка при отправке формы');
+      console.log(error);
+    }
     console.log('click');
   };
   return (
@@ -19,7 +39,13 @@ export const App: React.FC = () => {
           alt='Enter text'
         />
         <button onClick={onClickSend}>
-          <div>Заказать</div>
+          <div>
+            Заказать
+            <FontAwesomeIcon
+              className='send__icon'
+              icon={faPaperPlane}
+            />
+          </div>
         </button>
       </div>
     </div>
