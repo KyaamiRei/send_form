30895a750825cee15c709bc07abd697dc572cdcc
kyaami <kolyavasilenko2703@gmail.com>
2022-11-16 23:43:20 +0300

    init validate

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,54 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // подключени еaxios для отправки запросов на сервер
-// import axios from 'axios';
+import axios from 'axios';
 
 // подключение иконок font-awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 // подключение стилей
-import styles from './App.module.scss';
+import styles from './scss/App.module.scss';
 
 export const App: React.FC = () => {
   //состояние для текста в input
   const [inputValue, setInputValue] = useState<string>('');
+  const [validate, setValidate] = useState<boolean>(true);
 
   // функция для отправки данных по нажатию на кнопку
-  const onClickSend: () => void = () => {
+  const onClickSend = () => {
     try {
-      // отправка формы
-      // axios.post()
-      setInputValue('');
-      alert('Форма успешно отправлена');
+      if (validate) {
+        // отправка формы
+        axios.post('https://6375493d08104a9c5f96becf.mockapi.io/message', inputValue);
+        alert('Форма успешно отправлена');
+        setInputValue('');
+      }
     } catch (error) {
       // действия в случае ошибки при отправке формы
       alert('Произошла ошибка при отправке формы');
       console.log(error);
     }
-    console.log('click');
   };
+
+  useEffect(() => {
+    inputValue.length > 0 ? setValidate(true) : setValidate(false);
+  }, [inputValue]);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.send__form}>
         <input
+          className={validate ? '' : styles.is__valid}
           value={inputValue}
           onChange={(event) => setInputValue(event.target.value)}
           type='text'
-          placeholder='Введите номер...'
+          placeholder={`Введите номер...`}
           alt='Enter text'
         />
-        <button onClick={onClickSend}>
+        <button
+          disabled={!validate}
+          onClick={onClickSend}>
           <div>
             Заказать
             <FontAwesomeIcon
